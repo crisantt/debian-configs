03c8e8262ccb343b38bfe3c743e410fbdc7dad42
Cris <152991141+crscrused@users.noreply.github.com>
2024-11-10 02:14:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt install -y xserver-xorg-core xserver-xorg-input-libinput xserver-xorg-v
 ## sudo apt install xserver-xorg-video-amdgpu xserver-xorg-video-nouveau
 
 ## developer packages ##
-sudo apt install -y build-essential git pkg-config cmake zlib1g-dev libssl-dev libffi-dev libxcb1-dev libx11-dev libx11-xcb-dev libxrandr-dev libxinerama-dev libxft-dev autoconf automake libtool openjdk-17-jdk clangd wget curl zip
+sudo apt install -y build-essential pkg-config cmake zlib1g-dev libssl-dev libffi-dev libxcb1-dev libx11-dev libx11-xcb-dev libxrandr-dev libxinerama-dev libxft-dev autoconf automake libtool openjdk-17-jdk clangd wget curl zip
 
 ## fonts & appearance ##
 sudo apt install -y lxappearance fonts-dejavu fonts-font-awesome fonts-noto-core fonts-noto-cjk fonts-noto-color-emoji fonts-hack-ttf
