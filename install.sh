151c90aab40c800ab289c77818aad9edbba9426a
Cris <152991141+crscrused@users.noreply.github.com>
2024-11-11 02:40:03 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -63,7 +63,7 @@ done
 apt install feh picom rofi xclip dunst thunar firefox-esr pavucontrol -y
 
 ## developer packages ##
-apt install libx11-dev libxft-dev libxinerama-dev libxrandr-dev libxtst-dev libpango1.0-dev libxpm-dev -y
+apt install libx11-dev libxft-dev libxinerama-dev libxrandr-dev libxtst-dev libpango1.0-dev libxpm-dev libncurses5-dev -y
 apt install make gcc libtool wget -y
 apt-get install ninja-build gettext cmake unzip curl build-essential -y
 apt install clangd openjdk-17-jdk -y
