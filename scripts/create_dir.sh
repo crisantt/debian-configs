e60db7d6bce21c64c6e411817465de44d19baa49
Cris <152991141+crscrused@users.noreply.github.com>
2024-11-15 02:52:10 +0800

    Update create_dir.sh

--- scripts/create_dir.sh
+++ scripts/create_dir.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 ## Create Directories & Files ##
-cd $builddir
-mkdir -p /home/$username/github/suckless
-mkdir -p /home/$username/.config
-mkdir -p /home/$username/.fonts
-mkdir -p /home/$username/.themes
-mkdir -p /home/$username/.icons
-mkdir -p /home/$username/Pictures/Wallpapers
-cp -R dotwallpapers/* /home/$username/Pictures/Wallpapers/
-touch /home/$username/.xinitrc
-chown -R $username:$username /home/$username
+mkdir -p ~/github/suckless
+mkdir -p ~/.config
+mkdir -p ~/.fonts
+mkdir -p ~/.themes
+mkdir -p ~/.icons
+mkdir -p ~/Pictures/Wallpapers
