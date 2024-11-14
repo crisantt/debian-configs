fe866a45c043601293fbaef288308f87ca553297
Cris <152991141+crscrused@users.noreply.github.com>
2024-11-15 03:09:23 +0800

    Create edp.sh

---
+++ scripts/edp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## packages essentials ##
+apt install feh picom rofi xclip dunst thunar pavucontrol -y
+## developer packages ##
+apt install libx11-dev libxft-dev libxinerama-dev libxrandr-dev libxtst-dev libpango1.0-dev libxpm-dev libncurses5-dev -y
+## tools ##
+apt install make gcc libtool wget  -y
+apt-get install ninja-build gettext cmake unzip tar curl build-essential -y
+apt install clangd openjdk-17-jdk -y
