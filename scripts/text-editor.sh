af370c1e0e48ff2ee5f6ea2283d4c79c00f50dca
Cris <152991141+crscrused@users.noreply.github.com>
2024-11-15 04:14:47 +0800

    Update text-editor.sh

--- scripts/text-editor.sh
+++ scripts/text-editor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+username=$(id -u -n 1000)
 ## Install Neovim ##
-cd ~/github
+cd /home/$username/github
 git clone https://github.com/neovim/neovim.git
 cd neovim
 git checkout tags/stable
