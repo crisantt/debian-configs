121b44433d07b38c9bcb64b93867a2ae51734e82
Cris <152991141+crscrused@users.noreply.github.com>
2024-11-15 02:54:36 +0800

    Create dp.sh

---
+++ scripts/dp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Display Server Setup ##
+echo "Please Select Your Display Server"
+dpserver_option=("xorg" "wayland")
+select dpserver in "${dpserver_option[@]}"; do
+  if [ "$dpserver" = "xorg" ]; then
+    ## XORG ##
+    apt install xserver-xorg-core xserver-xorg-input-libinput x11-xserver-utils xinit -y
+    break
+  elif [ "$dpserver" = "wayland" ]; then
+    ## Wayland ##
+    apt install wayland-protocols xwayland -y
+    break
+  fi
+done
