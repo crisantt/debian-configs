5b4ddfa99a2df430415203c8797dc22a12ca649b
Cris <152991141+crscrused@users.noreply.github.com>
2024-11-15 04:17:02 +0800

    Update window-managers.sh

--- scripts/window-managers.sh
+++ scripts/window-managers.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
+username=$(id -u -n 1000)
 ### Setup Window Manager ###
 wm_option=("dwm" "bspwm")
 select wm in "${wm_option[@]}"; do
   if [ "$wm" = "dwm" ]; then
     # DWM
-    cd ~/github/suckless
+    cd /home/$username/github/suckless
     git clone https://git.suckless.org/dwm
     cd dwm
     make clean install
-    cd ~/github/suckless
+    cd /home/$username/github/suckless
     git clone https://git.suckless.org/st
     cd st
     make clean install
-    cd ~/github/suckless
+    cd /home/$username/github/suckless
     git clone https://git.suckless.org/dmenu
     cd dmenu
     make clean install
-    cd ~/
-    echo "picom -fb &" >> ~/.xinitrc
-    echo "dunst &" >> ~/.xinitrc
-    echo "exec dwm" >> ~/.xinitrc
+    cd /home/$username/
+    echo "picom -fb &" >> /home/$username/.xinitrc
+    echo "dunst &" >> /home/$username/.xinitrc
+    echo "exec dwm" >> /home/$username/.xinitrc
     break
   elif [ "$wm" = "bspwm" ]; then
     # BSPWM
     apt install bspwm sxhkd polybar
-    mkdir -p ~/.config/bspwm ~/.config/sxhkd
-    cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
-    cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
+    mkdir -p /home/$username/.config/bspwm /home/$username/.config/sxhkd
+    cp /usr/share/doc/bspwm/examples/bspwmrc /home/$username/.config/bspwm/
+    cp /usr/share/doc/bspwm/examples/sxhkdrc /home/$username/.config/sxhkd/
     chmod +x ~/.config/bspwm/bspwmrc
-    echo "picom -fb &" >> ~/.xinitrc
-    echo "dunst &" >> ~/.xinitrc
-    echo "exec bspwm" >> ~/.xinitrc
+    echo "picom -fb &" >> /home/$username/.xinitrc
+    echo "dunst &" >> /home/$username/.xinitrc
+    echo "exec bspwm" >> /home/$username/.xinitrc
     break
   fi
 done
