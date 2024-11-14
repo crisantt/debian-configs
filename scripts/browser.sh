aa27a405d688356c800426459247831e0504ac66
Cris <152991141+crscrused@users.noreply.github.com>
2024-11-15 03:14:14 +0800

    Create browser.sh

---
+++ scripts/browser.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ~/softwares
+wget https://github.com/Alex313031/thorium/releases/download/M128.0.6613.189/thorium-browser_128.0.6613.189_SSE3.deb
+apt install ./thorium-browser_128.0.6613.189_SSE3.deb -y
