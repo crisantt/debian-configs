4a0c496c5dbf4374f53f9c2237028432b51060a4
Cris <152991141+crisantt@users.noreply.github.com>
2024-11-16 03:57:37 +0800

    Update browser.sh

--- scripts/browser.sh
+++ scripts/browser.sh
@@ -3,4 +3,4 @@ username=$(id -u -n 1000)
 ## Installs Thorium ##
 cd /home/$username/softwares
 wget https://github.com/Alex313031/thorium/releases/download/M128.0.6613.189/thorium-browser_128.0.6613.189_SSE3.deb
-apt install ./thorium-browser_128.0.6613.189_SSE3.deb -y
+sudo apt install ./thorium-browser_128.0.6613.189_SSE3.deb -y
