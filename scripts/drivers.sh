5841b6826f77e2cbe0dfd2fe74b221518385b925
Cris <152991141+crisantt@users.noreply.github.com>
2024-11-16 03:58:28 +0800

    Update drivers.sh

--- scripts/drivers.sh
+++ scripts/drivers.sh
@@ -4,16 +4,16 @@ echo "Please Select Your GPU Driver/Tools"
 gpu_option=("intel" "amd" "nvidia-open" "nvidia-proprietary")
 select gpu in "${gpu_option[@]}"; do
   if [ "$gpu" = "intel" ]; then
-    apt install xserver-xorg-video-intel -y
+    sudo apt install xserver-xorg-video-intel -y
     break
   elif [ "$gpu" = "amd" ]; then
-    apt install xserver-xorg-video-amdgpu -y
+    sudo apt install xserver-xorg-video-amdgpu -y
     break
   elif [ "$gpu" = "nvidia-open" ]; then
-    apt install xserver-xorg-video-nouveau -y
+    sudo apt install xserver-xorg-video-nouveau -y
     break
   elif [ "$gpu" = "nvidia-proprietary" ]; then
-    apt install nvidia-cuda-dev nvidia-cuda-toolkit -y
+    sudo apt install nvidia-cuda-dev nvidia-cuda-toolkit -y
     break
   fi
 done
