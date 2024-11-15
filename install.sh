961325456fbffa074175d349d2f71d3483ebc625
Cris <152991141+crisantt@users.noreply.github.com>
2024-11-16 03:56:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-## Check if Script is Run as Root ##
-if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
-  exit 1
-fi
-
 username=$(id -u -n 1000)
 builddir=$(pwd)
 
 ## UPDATE ##
-apt update
-apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
 
 ## Create Directories & Files ##
 cd $builddir
