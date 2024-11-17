c508cb6b4616617d15e4ee41f082baaed51fda30
Cris <152991141+crisantt@users.noreply.github.com>
2024-11-17 20:05:05 +0800

    Update alacritty.sh

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -9,7 +9,7 @@ rustup override set stable
 rustup update stable
 cargo build --release
 infocmp alacritty
-sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp target/release/alacritty /usr/local/bin
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
