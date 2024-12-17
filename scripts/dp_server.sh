dcefb8c52e778f85aa62d021ee435a9a66cfd767
Cris <152991141+crisantt@users.noreply.github.com>
2024-12-17 10:17:04 +0000

    Update dp_server.sh

--- scripts/dp_server.sh
+++ scripts/dp_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## Display Server Setup ##
-echo "Please Select Your Display Server"
+echo "Please Select Your Display Server/Compositor"
 dpserver_option=("xorg" "wayland")
 select dpserver in "${dpserver_option[@]}"; do
   if [ "$dpserver" = "xorg" ]; then
@@ -9,7 +9,7 @@ select dpserver in "${dpserver_option[@]}"; do
     break
   elif [ "$dpserver" = "wayland" ]; then
     ## Wayland ##
-    sudo apt install glslang-tools libcairo2-dev libcap-dev libdbus-1-dev libdisplay-info-dev libevdev-dev libgdk-pixbuf2.0-dev libinput-dev libjson-c-dev libliftoff-dev libpam0g-dev libpango1.0-dev libpcre2-dev libpixman-1-dev libseat-dev libsystemd-dev libvulkan-dev libwayland-dev libwayland-egl1 libwlroots-dev libxcb-ewmh-dev libxkbcommon-dev meson pkgconf scdoc tree wayland-protocols xwayland -y
+    sudo apt install wayland-protocols wayland-utils wl-clipboard xdg-desktop-portal-wlr libwlroots-dev xwayland qtwayland5 policykit-1-gnome network-manager-gnome network-manager libtgtk-layer-shell-dev
     break
   fi
 done
